a438cb03b93ad6c2331e1b56eabc137d226e2631
Pragya Sharma <120582132+gitprag-ya@users.noreply.github.com>
2023-11-15 02:13:26 +0530

    Update week2.py

--- week2.py
+++ week2.py
@@ -1,1 +1,16 @@
-
+def binarySearchIndexAndComparisons(l, k):
+    left = 0
+    right = len(l)-1
+    ct = 0
+    if l == []:
+        return (False, 1)
+    while left<=right:
+        mid = (left + right)//2
+        ct = ct + 1
+        if l[mid]==k:
+            return(True, ct)
+        if l[mid]>k:
+            right = mid-1
+        if l[mid]<k:
+            left = mid+1
+    return (False, ct)
